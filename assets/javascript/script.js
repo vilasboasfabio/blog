ecdb880c99cec4c0dd8e82ba66bb021f2df05012
Cumpri <rafaelcumpri.senai564@gmail.com>
2023-03-17 20:31:41 -0300

    correção script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,42 +13,42 @@ function sair() {
 }
 
 function passar1() {
-    var destaque2 = window.document.querySelector('section#sec1')
+    var destaque2 = window.document.querySelector('a#sec1')
     destaque2.style.backgroundColor = '#5c5757'
     destaque2.style.outline = 'double whitesmoke'
     destaque2.style.transition = '1s'
 }
 
 function sair1() {
-    var destaque2 = window.document.querySelector('section#sec1')
+    var destaque2 = window.document.querySelector('a#sec1')
     destaque2.style.backgroundColor = 'black'
     destaque2.style.outline = 'none'
     destaque2.style.transition = '1s'
 }
 
 function passar2() {
-    var destaque3 = window.document.querySelector('section#sec2')
+    var destaque3 = window.document.querySelector('a#sec2')
     destaque3.style.backgroundColor = '#5c5757'
     destaque3.style.outline = 'double whitesmoke'
     destaque3.style.transition = '1s'
 }
 
 function sair2() {
-    var destaque3 = window.document.querySelector('section#sec2')
+    var destaque3 = window.document.querySelector('a#sec2')
     destaque3.style.backgroundColor = 'black'
     destaque3.style.outline = 'none'
     destaque3.style.transition = '1s'
 }
 
 function passar3() {
-    var destaque4 = window.document.querySelector('section#sec3')
+    var destaque4 = window.document.querySelector('a#sec3')
     destaque4.style.backgroundColor = '#5c5757'
     destaque4.style.outline = 'double whitesmoke'
     destaque4.style.transition = '1s'
 }
 
 function sair3() {
-    var destaque4 = window.document.querySelector('section#sec3')
+    var destaque4 = window.document.querySelector('a#sec3')
     destaque4.style.backgroundColor = 'black'
     destaque4.style.outline = 'none'
     destaque4.style.transition = '1s'
